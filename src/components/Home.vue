bafed526170f711d5bc09b032e861c541871a99f
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-10-28 19:50:02 +0100

    add the total available funds on the home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,19 @@
 <template>
-	<h2>Home Component</h2>
+	<div>
+		<h2>Trade or View Your Portfolio</h2>
+		<h6>You may Save & Load Your Data</h6>
+		<h6>Click on the End Day to begin a New Day!</h6>
+		<hr>
+		<p>Your Fund: {{ funds }}</p>
+	</div>
 </template>
+
+<script>
+	export default{
+		computed:{
+			funds(){
+			return this.$store.getters.funds;
+			}
+		}
+	}
+</script>
