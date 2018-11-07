284fd6453429ae6b09924dfdfca6fd068288bfd1
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-11-07 07:58:46 +0100

    correct funds display and complete the end day method

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,7 @@
 		<h6>You may Save & Load Your Data</h6>
 		<h6>Click on the End Day to begin a New Day!</h6>
 		<hr>
-		<p>Your Fund: {{ funds }}</p>
+		<p>Your Fund: {{ funds | currency }}</p>
 	</div>
 </template>
 
