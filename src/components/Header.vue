c51592ef5ec0dda27a7112eb5b173f900453da5f
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-11-07 12:08:55 +0100

    post our data to the firebase realtime db using the put method

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -24,7 +24,7 @@
               aria-expanded="false">
               Save & Load<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Save Data</a></li>
+            <li><a href="#" @click="saveData">Save Data</a></li>
             <li><a href="#">Load Data</a></li>
           </ul>
         </li>
@@ -55,6 +55,14 @@
 
       endDay(){
           this.randomizeStocks();
+      },
+      saveData(){
+        const data = {
+          funds: this.$store.getters.funds,
+          stockPortfolio: this.$store.getters.stockPorfolio,
+          stocks: this.$store.getters.stocks
+        };
+        this.$http.put('data.json', data);
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import  VueRouter  from 'vue-router';
+import  VueResource from 'vue-resource';
 import { routes } from './routes';
 import store from './store/store';
 
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
-vue.http.options.root = 'https://stock-trader-34e57.firebaseio.com/';
+Vue.http.options.root = 'https://stock-trader-34e57.firebaseio.com/';
 Vue.filter('currency', (value) => {
 	return '$' + value.toLocaleString();
 })
